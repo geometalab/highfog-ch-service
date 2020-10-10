0e71e17e4f4b5fe5635a773598eea62d1fc912ca
Nicola Jordan <Nicola Jordan>
2020-10-10 10:30:05 +0200

    adapt to new python; new docker environment; clean up; robustify updater; included external services alternatives

--- backend/scripts/update_heights.py
+++ backend/scripts/update_heights.py
@@ -1,14 +1,15 @@
-'''
+"""
 Created: 22.04.2015
 @author: Dennis Ligtenberg
 Script for updating poi heights using the elevation webservice
 Executing this will take a very long time!
-'''
+"""
 import ast
-import urllib2
 import os
+import requests
 
 from pyproj import Proj, transform
+from pyproj import Transformer
 from geoalchemy2 import func
 
 from config.ext_config import ELEVATION_SERVICE_URL
@@ -21,29 +22,41 @@ db.app = app
 
 
 def update_heights(table):
-    query = db.session.query(func.ST_X(table.geometry),
-                             func.ST_Y(table.geometry), table.osm_id)
+    query = db.session.query(
+        func.ST_X(table.geometry), func.ST_Y(table.geometry), table.osm_id
+    )
     for res in query:
         x = res[0]
         y = res[1]
-        proj1 = (Proj(init='epsg:3857'))
-        proj2 = (Proj(init='epsg:4326'))
-        x, y = transform(proj1, proj2, x, y)
+        transformer = Transformer.from_crs("epsg:3857", "epsg:4326")
+        x, y = transformer.transform(x, y)
+        print(x, y)
 
         try:
-            url = ELEVATION_SERVICE_URL + '?lat=' + \
-                str(y) + '&lon=' + str(x) + ''
-            response = urllib2.urlopen(url).read()
-            heightdict = ast.literal_eval(response)
+            url = ELEVATION_SERVICE_URL + "?lat=" + str(x) + "&lon=" + str(y) + ""
+            with requests.get(url) as response:
+                heightdict = response.json()
         # Set height to 0 if a HTTPError occurs
-        except urllib2.HTTPError:
-            heightdict = {'geometry': {'coordinates': [0, 0, 0]}}
-        print(heightdict)
+        except Exception as e:
+            print(e)
+            heightdict = {"geometry": {"coordinates": [0, 0, 0]}}
         result = db.session.query(table).filter(table.osm_id == res[2]).first()
-        result.height = heightdict['geometry']['coordinates'][2]
+        result.height = heightdict["geometry"]["coordinates"][2]
         db.session.commit()
 
 
-update_heights(Peak)
-update_heights(PublicTransport)
-print('Done!')
+def run_update():
+    # TODO: use logging and log error correctly
+    try:
+        update_heights(Peak)
+    except Exception as e:
+        print(e)
+
+    try:
+        update_heights(PublicTransport)
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    run_update()
